666b3d172214317990467b5d3ac9b43c9c7463ab
Álvaro <itoito@gmail.com>
2014-09-24 22:29:06 +0200

    Update OldMain.java

--- src/com/alcedomoreno/urjc/aa/greedy/OldMain.java
+++ src/com/alcedomoreno/urjc/aa/greedy/OldMain.java
@@ -5,31 +5,34 @@ public class OldMain {
 	public static void main( String[] args ) throws Exception{
 		int[] c = {0,7,5,2,16,11,15,23,12,24};
 		int[] f = {3,8,11,18,20,21,24,24,25,29};
-		int i = 0;
-		for ( boolean b:selecActividades(c, f) )
-			System.out.println( i++ + "-" + b );
+		int[] i = {3,2,4,7,8,0,9,6,5,1};
+		int w = 0;
+		for ( boolean b:selecActividades(i, c, f) )
+			System.out.println( w++ + "-" + b );
 	}
 	
-	public static boolean[] selecActividades(int[] c, int[] f) throws Exception{
+	public static boolean[] selecActividades(int[] i, int[] c, int[] f) throws Exception{
 		boolean[] s = new boolean[ c.length ];
 		
-		s[ 0 ] = true;
+		s[ i[0] ]= true;
 		
-		int i = 0;
+		int x = 0;
 		
 		for ( int j=1; j<c.length; j++ ){
 
-			if ( !isInRange( c[j],c[i],f[i] ) && !isInRange( f[j],c[i],f[i] ) ){
-				s[ j ] = true;
-				i = j;
+			if ( isInRange( f[j], f[x], c[j], c[x] ) ){
+				s[ i[ j ] ] = true;
+				x = j;
 			} else
-				s[ j ] = false;
+				s[ i[ j ] ] = false;
 		}
 		
 		return s;
 	}
 	
-	private static boolean isInRange( int date, int init, int end ){
-		return ( date > init && date < end );
+	
+	private static boolean isInRange( int a, int b, int c, int d ){
+		return ( ( a > b && c > d ) ||
+				( a == b && c > d ) );
 	}
 }
